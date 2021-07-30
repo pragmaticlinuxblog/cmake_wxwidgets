7991d259cbee6fb8cddb48b323202f0275c69963
PragmaticLinux <info@pragmaticlinux.com>
2021-07-30 16:45:50 +0200

    Minor refactoring and commenting.

--- source/main.cpp
+++ source/main.cpp
@@ -1,14 +1,37 @@
+/************************************************************************************//**
+* \file         main.cpp
+* \brief        Application source file.
+*
+****************************************************************************************/
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
 #include "main.h"
 #include "mainwindow.h"
 
+
+/****************************************************************************************
+* Macro definitions
+****************************************************************************************/
 /* Macro which creates the application instance and starts it. */
 IMPLEMENT_APP(MyApp)
 
+
+/************************************************************************************//**
+** \brief     Called upon application initialization. 
+** \return    True if the initialization was successful. False if anything failed, which
+**            prevents the application from continuing.
+**
+****************************************************************************************/
 bool MyApp::OnInit()
 {
   /* Create and show the application's main window. */
-  MainWindow *mainWindow = new MainWindow(wxT("MyApp"));
+  MainWindow *mainWindow = new MainWindow(wxT("MyApp"), 640, 480);
   mainWindow->Show(true);
 
   return true;
-}
+} /*** end of OnInit ***/
+
+
+/*********************************** end of main.cpp ***********************************/
--- source/mainwindow.cpp
+++ source/mainwindow.cpp
@@ -1,8 +1,29 @@
+/************************************************************************************//**
+* \file         mainwindow.cpp
+* \brief        Application's main window source file.
+*
+****************************************************************************************/
+
+/****************************************************************************************
+* Include files
+****************************************************************************************/
 #include "mainwindow.h"
 
-MainWindow::MainWindow(const wxString& title)
-  : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(640, 480))
+
+/************************************************************************************//**
+** \brief     Constructor of the application's main window.
+** \param     title Window title.
+** \param     width Width of the window in pixels.
+** \param     height Height of the window in pixels.
+**
+****************************************************************************************/
+MainWindow::MainWindow(const wxString& title, int width, int height)
+  : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(width, height))
 {
   /* Center the window on the screen. */
   Centre();
-}
+} /*** end of MainWindow ***/
+
+
+/*********************************** end of mainwindow.cpp *****************************/
+
